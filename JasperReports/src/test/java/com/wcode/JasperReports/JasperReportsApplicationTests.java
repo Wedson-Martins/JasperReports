e9db1661e66d4c95090e95fec5edab9066e6b2c2
Wedson Martins <wedsoncontato@gmail.com>
2021-04-21 13:33:50 -0300

    JasperReport

---
+++ JasperReports/src/test/java/com/wcode/JasperReports/JasperReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcode.JasperReports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
